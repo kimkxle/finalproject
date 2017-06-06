eaeb8cb9efd0c32b16fc17d2ecbd895df3664fe2
kimkxle <kimkxle@gmail.com>
2017-06-06 00:19:09 -0700

    final project

--- Final_Project/js/script.js
+++ Final_Project/js/script.js
@@ -18,8 +18,33 @@ var form = document.getElementById("goals-form");
 form.addEventListener('submit', calculate, false);
 
 /*
-var calculate1 = function(event1) {
-  event1.preventDefault();
+// Get all the keys from document
+var keys = document.querySelectorAll('.contrainer');
+var add = false;
+
+// Add onclick event to all the keys and perform operations
+for(var i = 0; i < keys.length; i++) {
+  keys[i].onclick = function(e) {
+    // Get the input and button values
+    var input = document.querySelector('#total');
+    var inputVal = input.innerHTML;
+    var submitBtn = this.innerHTML;
+    var clearBtn = this.innterHTML;
+
+// Now, append the key values (submitBtn to the input string and finally use javascript's eval function to get the result// If clear key is pressed, erase everything
+    if(clearBtn == 'clear') {
+      input.innerHTML = '';
+      add = false;
+    }
+    
+    // If eval key is pressed, calculate and display the result
+    else if(submitBtn == 'submit') {
+      var equation = inputVal;
+*/
+
+/*
+var calculate = function(event1) {
+event1.preventDefault();
   var quantity1 = document.getElementById("quantity1").value;
   var target1 = document.getElementById("target1").value;
   var logged1 = document.getElementById("logged1").value;
@@ -27,7 +52,9 @@ var calculate1 = function(event1) {
 }
 var form1 = document.getElementById("goals-form1");
 form1.addEventListener('submit1', calculate1, false);
+*/
 
+/*
 var calculate2 = function(event2) {
   event2.preventDefault();
   var quantity2 = document.getElementById("quantity2").value;
@@ -39,11 +66,18 @@ var form2 = document.getElementById("goals-form2");
 form2.addEventListener('submit2', calculate2, false);
 */
 
+//clear form
   $('#clear').click(function(event3) {
     event3.preventDefault();
     $('input').each(function(){
       $(this).closest('input').remove();
 
+/*
+$('#clear').click(function(event3) {
+    event3.preventDefault();
+    $('input').input.innerHTML = '';
+    add = false;
+*/
 
   });
 
